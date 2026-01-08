9eea11d1f7f362bbf419f368d677386692f9fa00
shariar17 <shariarhossain23@gmail.com>
2026-01-09 00:10:06 +0600

    add test

--- framework/__init__.py
+++ framework/__init__.py
@@ -7,6 +7,7 @@ from .handlers import CommonHandlers
 from .utils import json_response
 from .constants import HttpStatus
 from .middleware import Middleware
+from .response import Response
 
 __all__ = [
     'Application',
@@ -15,5 +16,6 @@ __all__ = [
     'json_response',
     'HttpStatus',
     'Middleware'
+    "Response"
 ]
 
--- test_framework.py
+++ test_framework.py
@@ -225,3 +225,52 @@ def test_class_based_handlers_still_work(api, client):
     # PUT not implemented - should raise AttributeError
     with pytest.raises(AttributeError):
         client.put("http://testserver/resource")
+
+
+
+# test_response_data
+
+def test_json_response_helper(api, client):
+    @api.route("/json")
+    def json_handler(req, resp):
+        resp.json = {"name": "PoridhiFrame"}
+
+    response = client.get("http://testserver/json")
+    json_body = response.json()
+
+    assert response.headers["Content-Type"] == "application/json"
+    assert json_body["name"] == "PoridhiFrame"
+
+def test_html_response_helper(api, client):
+    @api.route("/html")
+    def html_handler(req, resp):
+        resp.html = api.template("index.html", context={"title": "Best Title", "name": "Best Name"})
+
+    response = client.get("http://testserver/html")
+
+    assert "text/html" in response.headers["Content-Type"]
+    assert "Best Title" in response.text
+    assert "Best Name" in response.text
+
+def test_text_response_helper(api, client):
+    response_text = "Just Plain Text"
+
+    @api.route("/text")
+    def text_handler(req, resp):
+        resp.text = response_text
+
+    response = client.get("http://testserver/text")
+
+    assert "text/plain" in response.headers["Content-Type"]
+    assert response.text == response_text
+
+def test_manually_setting_body(api, client):
+    @api.route("/body")
+    def text_handler(req, resp):
+        resp.body = b"Byte Body"
+        resp.content_type = "text/plain"
+
+    response = client.get("http://testserver/body")
+
+    assert "text/plain" in response.headers["Content-Type"]
+    assert response.text == "Byte Body"
